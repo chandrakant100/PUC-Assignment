6b7e635fdb1400ea824286f1dcef4782e88a8bfd
chandrakant100 <chandrakant.barcelona@gmail.com>
2019-11-09 21:05:04 +0530

    edited some mistakes

--- assingment11/two.c
+++ assingment11/two.c
@@ -4,19 +4,12 @@ int main()
 {
 	char a[100];
 	int i=0,count=0;
-	printf("Enter the string:");
+	printf("Enter the string:\n");
 	fgets(a,100,stdin);
-	if((a[0]=='A'|| a[0]=='E' || a[0]=='I'|| a[0]=='O'||a[0]=='U'|| a[0]=='a'|| a[0]=='e'|| a[0]=='i'||a[0]=='o'||a[0]=='u') && a[1]==' ')
-		count=1;
-	i=1;
 	while(a[i]!='\0')
 		{
-			if(a[i]==' ')
-				{
-					if(a[i+1]=='A'|| a[i+1]=='E' || a[i+1]=='I'|| a[i+1]=='O'||a[i+1]=='U'|| a[i+1]=='a'|| a[i+1]=='e'|| a[i+1]=='i'||a[i+1]=='o'||a[i+1]=='u')
-						if(a[i+2]==' ' || a[i+3]=='\0')
+			if(a[i]=='A'|| a[i]=='E' || a[i]=='I'|| a[i]=='O'||a[i]=='U'|| a[i]=='a'|| a[i]=='e'|| a[i]=='i'||a[i]=='o'||a[i]=='u')
 							count++;
-				}
 			i++;
 		}
 	printf("\nNO. of vowels present %d",count);
